ec1d4dabe580ec9139d2e3d57dd7198bb3059f23
Abel <33330298+abeldf94@users.noreply.github.com>
2018-10-28 23:17:12 +0000

    Fixed documentation

--- src/es/ull/etsii/cc/components/Tape.java
+++ src/es/ull/etsii/cc/components/Tape.java
@@ -5,16 +5,16 @@ package es.ull.etsii.cc.components;
  * Input tape for the automaton.
  */
 public class Tape {
-	
+
 	/** The input string. */
 	private String input;
-	
+
 	/** The pointer for get every character from input. */
 	private Integer pointer;
-	
+
 	/** The epsilon character. */
 	private final String EPSILON = ".";
-	
+
 	/**
 	 * Instantiates a new tape.
 	 */
@@ -22,7 +22,7 @@ public class Tape {
 		input = null;
 		pointer = -1;
 	}
-	
+
 	/**
 	 * Reset the tape to default.
 	 *
@@ -32,7 +32,7 @@ public class Tape {
 		setInput(input);
 		setPointer(0);
 	}
-	
+
 	/**
 	 * Gets the current character it's pointing.
 	 *
@@ -45,39 +45,20 @@ public class Tape {
 			return Character.toString(getInput().charAt(getPointer()));
 	}
 
-	/**
-	 *  Getters and Setters *.
-	 *
-	 * @return the input
-	 */
-	
+	/** Getters and Setters **/
+
 	public String getInput() {
 		return input;
 	}
 
-	/**
-	 * Sets the input.
-	 *
-	 * @param content the new input
-	 */
 	public void setInput(String content) {
 		this.input = content;
 	}
 
-	/**
-	 * Gets the pointer.
-	 *
-	 * @return the pointer
-	 */
 	public Integer getPointer() {
 		return pointer;
 	}
 
-	/**
-	 * Sets the pointer.
-	 *
-	 * @param pointer the new pointer
-	 */
 	public void setPointer(Integer pointer) {
 		this.pointer = pointer;
 	}
